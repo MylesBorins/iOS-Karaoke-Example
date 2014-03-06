011fdfbcbc4b2738ff0d63e1dfbef4471de4bbb6
Myles Borins <myles.borins@gmail.com>
2014-03-06 14:26:13 -0800

    Cocoa Pods ftw

---
+++ Karaoke-Example/Pods/Pods-Karaoke-Example-RKValueTransformers-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Karaoke_Example_RKValueTransformers : NSObject
+@end
+@implementation PodsDummy_Pods_Karaoke_Example_RKValueTransformers
+@end
---
+++ Karaoke-Example/Pods/Pods-Karaoke-Example-TransitionKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Karaoke_Example_TransitionKit : NSObject
+@end
+@implementation PodsDummy_Pods_Karaoke_Example_TransitionKit
+@end
