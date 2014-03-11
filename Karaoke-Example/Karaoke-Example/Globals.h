810518bb51c8214ca90679fa6e6cf28665831b5f
Myles Borins <myles.borins@gmail.com>
2014-03-10 18:42:17 -0700

    Update Globals to support graphics engine

--- Karaoke-Example/Karaoke-Example/Globals.h
+++ Karaoke-Example/Karaoke-Example/Globals.h
@@ -12,6 +12,7 @@
 #include <iostream>
 #import "mo_audio.h"
 #import "mo_gfx.h"
+#import "SceneGraph.h"
 
 using namespace std;
 
@@ -25,15 +26,16 @@ class Globals
 {
 public:
     // global variables
+    static GLfloat waveformWidth;
     static GLfloat gfxWidth;
     static GLfloat gfxHeight;
     
-    // globals for recording
-    static BOOL recording;
-    static BOOL playing;
-    static int recordingLength;
-    static int playHead;
-    static Float32 * recordingBuffer;
+    // globals for phsyics
+    static GLfloat scaler;
+    static GLfloat boundVelocity;
+    
+    // top level root simulation
+    static SceneGraph * graph;
     
     // colors
     static Vector3D ourWhite;
@@ -45,6 +47,23 @@ public:
     static Vector3D ourYellow;
     static Vector3D ourSoftYellow;
     static Vector3D ourPurple;
+    
+    // globals for recording
+    static BOOL recording;
+    static BOOL playing;
+    static int recordingLength;
+    static int playHead;
+    static Float32 * recordingBuffer;
+    
+    // simple scheduler
+    static UInt32 clock;
+    static double tempo;
+    static UInt32 thirtysecond;
+    static UInt32 sixteenth;
+    static UInt32 eigth;
+    static UInt32 quarter;
+    static UInt32 half;
+    static UInt32 whole;
 };
 
 #endif /* defined(__Karaoke_Example__Globals__) */
--- Karaoke-Example/Karaoke-Example/Globals.mm
+++ Karaoke-Example/Karaoke-Example/Globals.mm
@@ -9,18 +9,27 @@
 #include "Globals.h"
 
 // global variables
+GLfloat Globals::waveformWidth = 2;
 GLfloat Globals::gfxWidth = 1024;
 GLfloat Globals::gfxHeight = 640;
 
-// globals for recording
+// Scene Graph
+SceneGraph * Globals::graph = NULL;
 
-// globals for recording
-BOOL Globals::recording = false;
-BOOL Globals::playing = false;
-int Globals::recordingLength = 0;
-int Globals::playHead = 0;
+// Physics stuff
+GLfloat Globals::scaler = 1;
+GLfloat Globals::boundVelocity = 1.5;
 
-Float32 * Globals::recordingBuffer = new float[MAX_RECORD_LENGTH];
+// Set defaults for scheduler
+UInt32 Globals::clock = 0;
+double Globals::tempo = 128;
+double beat = (SAMPRATE * 60) / Globals::tempo;
+UInt32 Globals::whole = beat * 4;
+UInt32 Globals::half = beat * 2;
+UInt32 Globals::quarter = beat;
+UInt32 Globals::eigth = beat / 2;
+UInt32 Globals::sixteenth = beat / 4;
+UInt32 Globals::thirtysecond = beat / 8;
 
 // colors
 Vector3D Globals::ourWhite( 1, 1, 1 );
@@ -32,3 +41,11 @@ Vector3D Globals::ourGray( .4, .4, .4 );
 Vector3D Globals::ourYellow( 1, 1, .25 );
 Vector3D Globals::ourSoftYellow( .7, .7, .1 );
 Vector3D Globals::ourPurple( .6, .25, .6 );
+
+// globals for recording
+BOOL Globals::recording = false;
+BOOL Globals::playing = false;
+int Globals::recordingLength = 0;
+int Globals::playHead = 0;
+
+Float32 * Globals::recordingBuffer = new float[MAX_RECORD_LENGTH];
